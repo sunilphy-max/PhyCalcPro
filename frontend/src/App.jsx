53093c9f1855422ce954d9ad378b960a8b91208f
PhyCalc <sunilphy@gmail.com>
2026-02-28 23:58:30 -0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,13 @@
-import React, { useEffect, useState } from "react";
+const API_URL = import.meta.env.VITE_API_URL;
 
-export default function App() {
-  const [msg, setMsg] = useState("");
-  const [plot, setPlot] = useState("");
+useEffect(() => {
+  fetch(`${API_URL}/hello`)
+    .then(res => res.json())
+    .then(data => setMsg(data.message));
+}, []);
 
-  // Fetch hello endpoint
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/hello")
-      .then(res => res.json())
-      .then(data => setMsg(data.message));
-  }, []);
-
-  // Fetch plot endpoint
-  useEffect(() => {
-    fetch("http://127.0.0.1:8000/plot-test")
-      .then(res => res.json())
-      .then(data => setPlot(`data:image/png;base64,${data.plot}`));
-  }, []);
-
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to PhyCalcPro</h1>
-      <p>{msg}</p>
-      {plot && <img src={plot} alt="Test Plot" />}
-    </div>
-  );
-}
+useEffect(() => {
+  fetch(`${API_URL}/plot-test`)
+    .then(res => res.json())
+    .then(data => setPlot(`data:image/png;base64,${data.plot}`));
+}, []);
