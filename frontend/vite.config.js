00221c6eadb3a9ce88846f593d8a719e5c2fe6e8
PhyCalc <sunilphy@gmail.com>
2026-03-01 19:27:19 -0500

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "../backend/static"  // React build goes directly into backend static folder
-  }
+  base: "/", // important for Render deployment
 });
