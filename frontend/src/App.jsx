4fe47691b23a044de98f7909632e9b02ff1ba24c
PhyCalc <sunilphy@gmail.com>
2026-03-04 19:56:30 -0500

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,10 @@
-import React, { useEffect, useState } from "react";
-import PlotCard from "./components/PlotCard";
+import React from "react";
 import BeamCalculator from "./components/BeamCalculator";
 
 function App() {
   return (
     <div>
       <BeamCalculator />
-    </div>
-  );
-}
-function App() {
-  const [result, setResult] = useState(null);
-
-  useEffect(() => {
-    fetch("/api/example?x=4")
-      .then((res) => res.json())
-      .then((data) => setResult(data))
-      .catch((err) => console.error(err));
-  }, []);
-
-  return (
-    <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to PhyCalcPro</h1>
-      {result && (
-        <div>
-          <p>Example Calculation: x = {result.x}, y = {result.y.toFixed(3)}</p>
-          <PlotCard src={result.plot} />
-        </div>
-      )}
     </div>
   );
 }
